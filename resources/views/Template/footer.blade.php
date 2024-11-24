acb16cada0866e51be3caaf533bd82d461c7fc40
Atar031308 <rezqiavatar1@gmail.com>
2024-11-24 09:43:21 +0700

    perbaiki profile

--- resources/views/Template/footer.blade.php
+++ resources/views/Template/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
                     <div class="container my-auto">
                         <div class="copyright text-center my-auto">
-                            <span>2024 &copy; SiRebon. Dinas Komunikasi, Informatika & Statistik.</span>
+                            <span>2024 &copy; Sirepal. Dinas Komunikasi, Informatika & Statistik.</span>
                         </div>
                     </div>
                 </footer>
